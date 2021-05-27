4893a8c0ab25d189396f9bb6490db24423f22834
mrepol742 <mrepol742@protonmail.com>
2021-05-27 13:31:33 +0800

    Initial commit

---
+++ Pixel_2_API_21.avd.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd "C:\Users\mrepol742\AppData\Local\Android\Sdk\emulator"
+emulator -avd Pixel_2_API_21
+PAUSE
---
+++ mrepol742.avd.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd "C:\Users\mrepol742\AppData\Local\Android\Sdk\emulator"
+emulator -avd mrepol742
+PAUSE
