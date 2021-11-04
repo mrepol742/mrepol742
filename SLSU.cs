0ffb65285c7056cebec4aa15c469fe0baf52f06c
mrepol742 <mrepol742@protonmail.com>
2021-11-04 23:43:26 +0800

    Initial Commit

---
+++ SLSU.cs
@@ -0,0 +1,11 @@
+using System;
+namespace SLSU {
+    class Program {
+        static void Main(string[] args) {
+        Console.WriteLine("MISSION\n");
+        Console.WriteLine("A high quality corporate university of science and technology innovation.\n\n");
+        Console.WriteLine("VISION\n");
+        Console.WriteLine("SLSU will: Developed Science, Technology and Innovation leaders, and professionals;\nProduce high-impact technologies from research and innovations;\nContribute to sustainable development through responsive community engagement programs;\nGenerate revenues to be self-sufficient and financially-visible..");
+        }
+    }
+}
