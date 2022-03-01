6b40133823c3b737a70922e1c8ec3d13d15ad326
mrepol742 <mrepol742@gmail.com>
2022-03-01 10:43:33 +0800

    Initial Commit

--- cli.js
+++ cli.js
@@ -1,9 +1,10 @@
-import minimist from 'minimist'
-import pkg from '.'
+const minimist = require('minimist');
+const pkg = require('.');
 
 const options = {
   alias: { json: 'j' }
-}
-const argv = minimist(process.argv.slice(2), options)
+};
 
-console.log(pkg(argv))
+const argv = minimist(process.argv.slice(2), options);
+
+console.log(pkg(argv));
